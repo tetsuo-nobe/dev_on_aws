bb02e3a7252ec905cb3fc6c7723be4b5c3f8b450
Tetsuo Nobe <tnobe@amazon.co.jp>
2023-07-06 08:29:14 +0900

    add get env value on Lambda work

--- v4/Lambda/lambda_function.py
+++ v4/Lambda/lambda_function.py
@@ -1,15 +1,19 @@
 from datetime import datetime
+import os
 
 # ハンドラ関数の外側で日時データを取得
 outside_handler = datetime.now()
 
 def lambda_handler(event, context):
     # ハンドラ関数の内側で日時データを取得
-    inside_handler = datetime.now()
+    inside_handler = datetime.now()  
     
     # 取得した日時データをログ出力
     print('outside_handler :' + str(outside_handler))
     print('inside_handler  :' + str(inside_handler))
+    
+    # 環境変数 MSG から値を取得
+    message = os.environ.get('MSG', 'Hello')
     
     # context オブジェクトからいくつかの値をログ出力
     print("Lambda function ARN:", context.invoked_function_arn)
@@ -19,5 +23,5 @@ def lambda_handler(event, context):
     # event オブジェクトから name の値を取得して return のペイロードに含める
     return {
         'statusCode': 200,
-        'body': 'Hello! '  + event['name']
+        'body': message + ' '  + event['name']
         }
